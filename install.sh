39421c55e2c0317ecf6788c1544a84e914461a88
joachim verbeke <joachim.verbeke@outlook.com>
2014-07-23 15:37:40 +0200

    first commit, dotfiles

---
+++ install.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+# Get current dir (to run this script from anywhere)
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Bunch of symlinks
+log_message "symlinking.."
+ln -sfhv "$DOTFILES_DIR/runcom/.bash_profile"" ~
+ln -sfhv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfhv "$DOTFILES_DIR/git/.gitignore_global" ~
+
+#if [[ ! -f ~/.envvars.rc ]]; then
+#    echo "export DOTFILESDIR=${DOTFILES_DIR}" > ~/.envvars.rc
+#else
+#    if [[ $(cat ~/.envvars.rc | grep DOTFILESDIR) ]]; then
+#        echo "export DOTFILESDIR=${DOTFILES_DIR}" >> ~/.envvars.rc
+#    else
+#        sed -i ~/.envvars.rc -e "s/export\sDOTFILESDIR=.*$/export DOTFILESDIR=${DOTFILES_DIR}/"
+#    fi
+#fi
+
+# install homebrew
+log_message "Brewing ALL THE THINGS.."
+if [[ ! $(which brew) ]]; then
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+else
+    brew update
+    brew upgrade
+fi
+
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+# Install brew & brew-cask packages
+brew bundle "$DOTFILES_DIR/packages/Brewfile"
+brew bundle "$DOTFILES_DIR/packages/Caskfile"
+
+# nvm
+if [[ ! -f ~/.nvm/nvm.sh ]];then
+    LATEST_STABLE_NODE=$(curl -s  http://nodejs.org/dist/latest/ | ${DOTFILES_DIR}/bin/g_or_native grep "\.pkg" | ${DOTFILES_DIR}/bin/g_or_native sed -e 's/<[^>]*>//g' | ${DOTFILES_DIR}/bin/g_or_native cut -d ' ' -f 1 | ${DOTFILES_DIR}/bin/g_or_native sed -e 's/node-v//g' | ${DOTFILES_DIR}/bin/g_or_native sed -e 's/\.pkg//g')
+    log_message "Installing nvm and node v${LATEST_STABLE_NODE}"
+    git clone https://github.com/creationix/nvm.git ~/.nvm
+    source ~/.nvm/nvm.sh
+    nvm install ${LATEST_STABLE_NODE}
+    nvm use ${LATEST_STABLE_NODE}
+    nvm alias default ${LATEST_STABLE_NODE}
+fi
+
+# rvm
+if [[ ! -f ~/.rvm/scripts/rvm ]]; then
+    log_message "Installing rvm"
+    curl -sSL https://get.rvm.io | bash -s stable --ruby
+    source ~/.rvm/scripts/rvm
+fi
+
+# node modules
+log_message "Installing node modules.."
+npm install -g $(cat ${DOTFILES_DIR}/packages/node)
+
+# ruby gems
+log_message "Installing ruby gems.."
+gem install $(cat ${DOTFILES_DIR}/packages/ruby)
+
+#custom osx settings
+log_message "Setting custom OS-X Settings.."
+source ${DOTFILES_DIR}/osxdefaults.sh
+
+#log_message "Installing iTerm2 solarized colorschemes.."
+#open ${DOTFILES_DIR}/deps/solarized/iterm2-colors-solarized/Solarized\ Dark.itermcolors
+#open ${DOTFILES_DIR}/deps/solarized/iterm2-colors-solarized/Solarized\ Light.itermcolors
+
+# zsh
+log_message "Setting zsh (FTW) as shell.."
+ZSH=$(which zsh)
+chsh -s ${ZSH}
+sudo chsh -s ${ZSH}
+
+# fonts
+log_message "getting fonts"
+wget http://www.levien.com/type/myfonts/Inconsolata.otf -O /Library/Fonts/Inconsolata.otf
+wget https://gist.github.com/raw/1595572/Inconsolata-dz-Powerline.otf -O /Library/Fonts/Inconsolata-dz-Powerline.otf
+wget https://gist.github.com/raw/1595572/Menlo-Powerline.otf -O /Library/Fonts/Menlo-Powerline.otf
+wget https://gist.github.com/raw/1595572/mensch-Powerline.otf -O /Library/Fonts/mensch-Powerline.otf
+
+# Finished!!!
+log_message "Done, great success!!"
---
+++ runcom/.bash_profile
@@ -0,0 +1,21 @@
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# Resolve path until file is no longer a symlink
+PROFILE_SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$PROFILE_SOURCE" ]; do
+    DOTFILES_DIR="$( cd -P "$( dirname "$PROFILE_SOURCE" )" && pwd )"
+    PROFILE_SOURCE="$(readlink "$PROFILE_SOURCE")"
+    [[ $PROFILE_SOURCE != /* ]] && PROFILE_SOURCE="$DIR/$PROFILE_SOURCE"
+done
+DOTFILES_DIR="$( cd -P "$( dirname "$PROFILE_SOURCE" )/.." && pwd )"
+
+# Finally we can evaluate the actual files
+for DOTFILE in `find ~/Projects/dotfiles/system`
+do
+    [ -f "$DOTFILE" ] && source "$DOTFILE";
+done;
+
+unset DOTFILE PROFILE_SOURCE DOTFILES_DIR
+
+cd ~
